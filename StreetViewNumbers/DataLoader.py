6ba8c6f5cb450858f2bf2efc3dce6a5aa95fb01d
Tom McClintock <tmcclintock89@gmail.com>
2020-02-13 16:24:28 -0500

    Data loader added.

---
+++ StreetViewNumbers/DataLoader.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.io import loadmat
+
+class DataLoader(object):
+    def __init__(
+            self,
+            input_path = "/Users/tmcclintock/Github/StreetViewNumbers/data/"):
+        self._input_path = input_path
+
+    def load_training_data(self, fname = "train_32x32.mat"):
+        training_dataset = loadmat(
+            self._input_path + fname)        
+        train_X = training_dataset['X']
+        train_y = np.squeeze(training_dataset['y'])
+        train_X = np.moveaxis(train_X, -1, 0)
+        return train_X, train_y
+
+    def load_testing_data(self, fname = "test_32x32.mat"):
+        test_dataset = loadmat(
+            self._input_path + fname)        
+        test_X = test_dataset['X']
+        test_y = np.squeeze(training_dataset['y'])
+        test_X = np.moveaxis(test_X, -1, 0)
+        return test_X, test_y
--- StreetViewNumbers/__init__.py
+++ StreetViewNumbers/__init__.py
@@ -1,4 +1,5 @@
 from .StreetViewNumbers import *
+from .DataLoader import *
 
 __author__ = "Tom McClintock <thmsmcclintock@gmail.com"
 __version__ = "0.1.0"
