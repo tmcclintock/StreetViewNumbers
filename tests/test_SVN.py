bd8cadd2f20174daa095e27d276bf179a34a1dd9
Tom McClintock <tmcclintock89@gmail.com>
2020-02-13 08:46:07 -0500

    Adding a smoke test and a temporary attributes test

---
+++ tests/test_SVN.py
@@ -0,0 +1,12 @@
+import StreetViewNumbers as SVN
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+def test_SVN():
+    #Smoke test
+    s = SVN.SVN()
+
+def test_SVN_has_attr():
+    s = SVN.SVN()
+    npt.assert_equal(True, hasattr(s, "att"))
